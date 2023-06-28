5721b4ecf132ecc0f12044678a5d65810fab51cc
khahtm <122286873+khahtm@users.noreply.github.com>
2023-06-28 12:56:38 +0700

    update

--- src/elements/blog/BlogContent.jsx
+++ src/elements/blog/BlogContent.jsx
@@ -1,19 +1,19 @@
 const BlogContent = [
     {
         images: '01',
-        title: ' Getting tickets to the big show',
-        category: 'Development'
+        title: ' Enhancing crop resilience and organic yields through groundbreaking discoveries.',
+        category: 'Biotech Breakthrough'
     },
     {
         images: '02',
-        title: 'A big ticket gone to be an interesting ',
-        category: 'Management'
+        title: 'Boosting productivity and organic integrity through innovative partnerships.',
+        category: 'Successful Collaboration'
     },
 
     {
         images: '03',
-        title: 'The Home of the Future Could Bebes',
-        category: 'Design'
+        title: 'Unveiling the future of sustainable agriculture with emerging trends and technologies',
+        category: 'Industry Insights'
     },
     {
         images: '04',
